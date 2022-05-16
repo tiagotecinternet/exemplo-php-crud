94083ef6f6ee2cc08e53c4c0da398b79e1b5ccae
tiagotecinternet <tiago.tecinternet22@gmail.com>
2022-05-16 11:58:45 -0300

    Início do INSERT de fabricantes

---
+++ fabricantes/inserir.php
@@ -0,0 +1,36 @@
+<?php
+/* Verificando se o botão do formulário
+foi acionado */
+if( isset($_POST['inserir']) ){
+    // Importando as funções e a conexão
+    require_once "../src/funcoes-fabricantes.php";
+
+    // Capturando o que foi digitado no campo nome
+    $nome = $_POST['nome'];
+
+    inserirFabricante($conexao, $nome);
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabricantes - Inserir</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Fabricantes | INSERT</h1>
+        <hr>
+        <form action="" method="post">
+            <p>
+                <label for="nome">Nome:</label>
+                <input type="text" name="nome" id="nome">
+            </p>
+            <button type="submit" name="inserir">
+                Inserir fabricante</button>
+        </form>
+    </div>
+</body>
+</html>
