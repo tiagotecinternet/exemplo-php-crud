0a541039831adb275be2a75d6df98b348c8c9ff4
tiagotecinternet <tiago.tecinternet22@gmail.com>
2022-05-18 08:55:28 -0300

    Função inserirFabricante

--- src/funcoes-fabricantes.php
+++ src/funcoes-fabricantes.php
@@ -16,10 +16,13 @@ function lerFabricantes(PDO $conexao):array {
 
 
 // Inserir um fabricante
-function inserirFabricante(PDO $conexao, string $nome) {
-    $sql = "INSERT INTO fabricantes(nome) VALUES('$nome')";
+function inserirFabricante(PDO $conexao, string $nome):void {
+    /* :qualquer_coisa named parameters */
+    $sql = "INSERT INTO fabricantes(nome) VALUES(:nome)";
     try {
         $consulta = $conexao->prepare($sql);
+
+        /* bindParam('nome do parametro', $variavel_com_valor, constante de verificação) */
         $consulta->bindParam(':nome', $nome, PDO::PARAM_STR);
         $consulta->execute();
     } catch (Exception $erro) {
